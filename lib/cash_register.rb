21a023cecc625743ee6b497e1ca03c7e4bcb383d
dainbrownlow08 <dain.brownlow@outlook.com>
2020-11-18 17:03:18 -0800

    final

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+require 'pry'
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total
+    
+    
+    def initialize(discount = 20)
+        @total = 0
+        @discount = discount
+        @items = []
+        @prices = []
+        
+    end
 
+    def add_item(title,price, quantity = 1)
+        self.total += price*quantity
+        if quantity > 1
+            @prices << price*quantity
+            quantity.times do
+                @items << title
+            end
+        else
+            @items << title
+            @prices << price
+        end
+    end
+
+    def apply_discount()
+        original_price = @total
+        self.total = self.total*(100 - self.discount).to_f/100
+        self.total == original_price ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total.to_i}."
+    end
+
+    def items
+        @items
+    end 
+
+    def void_last_transaction
+        self.total = self.total - @prices.pop
+    end
+    
+
+    
+
+    
+
+
+end
